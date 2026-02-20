544fea40cc11596d8ebc1e29300b856ebd610540
GICA <yuhaoqiang62@gmail.com>
2026-02-20 17:16:11 +0800

    Add files via upload

---
+++ global_configs.py
@@ -0,0 +1,44 @@
+import os
+import torch
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+TEXT_DIM = 0
+ACOUSTIC_DIM = 0
+VISUAL_DIM = 0
+DEVICE = torch.device("cuda:0")
+
+def set_dataset_config(dataset_name):
+    global TEXT_DIM, ACOUSTIC_DIM, VISUAL_DIM
+
+    dataset_configs = {
+        "mosi": {"ACOUSTIC_DIM": 74, "VISUAL_DIM": 47, "TEXT_DIM": 768},
+        "mosei": {"ACOUSTIC_DIM": 74, "VISUAL_DIM": 35, "TEXT_DIM": 768}
+    }
+
+    config = dataset_configs.get(dataset_name)
+    if config:
+        ACOUSTIC_DIM = config["ACOUSTIC_DIM"]
+        VISUAL_DIM = config["VISUAL_DIM"]
+        TEXT_DIM = config["TEXT_DIM"]
+    else:
+        raise ValueError(f"Invalid dataset name: {dataset_name}")
+import os
+import torch
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["WANDB_PROGRAM"] = "multimodal_driver.py"
+
+DEVICE = torch.device("cuda:0")
+
+# MOSI SETTING
+ACOUSTIC_DIM = 74
+VISUAL_DIM = 47
+TEXT_DIM = 768
+
+# MOSEI SETTING
+#ACOUSTIC_DIM = 74
+#VISUAL_DIM = 35
+#TEXT_DIM = 768
+
+XLNET_INJECTION_INDEX = 1
